be38877a8df06f211068366c94956be7d9d58780
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2022-12-09 17:26:38 +0300

    dz_N2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число A");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите чило B");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA>numB)
+{
+    Console.WriteLine("MAX число "+numA);
+    Console.WriteLine("MIN число "+numB);
+}
+else
+{
+    Console.WriteLine("MAX число "+numB);
+    Console.WriteLine("Min число "+numA);
+}
